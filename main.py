4495395749111b4ff5c3896f522426e6b32e2e4d
Nihil Hesara <nihilhesa@gmail.com>
2024-06-24 23:50:30 +0530

    Create the game space

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width = 600, height = 600)     # Setup the screen height and width
+screen.bgcolor("black")                     # Background color
+screen.title("Snake game")                  # popup screen title
+
+
+
+screen.exitonclick()
