e980e35bd2b24e2b6b5cfba5760194e2ad02dcb8
Nihil Hesara <nihilhesa@gmail.com>
2024-06-25 16:39:42 +0530

    Create the snake

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ screen.setup(width = 600, height = 600)     # Setup the screen height and width
 screen.bgcolor("black")                     # Background color
 screen.title("Snake game")                  # popup screen title
 
+starting_position = [(0,0), (-20,0), (-40,0)]
 
+# Create a 20px heigt and 60px width snake 
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)              # Createing each segment with it's axis value (starting_position)
 
 screen.exitonclick()
