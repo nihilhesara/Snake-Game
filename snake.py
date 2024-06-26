23b1c1e091903bba1d36d357c223f0c3303634f3
Nihil Hesara <nihilhesa@gmail.com>
2024-06-26 16:01:06 +0530

    if condition for snake direction

--- snake.py
+++ snake.py
@@ -1,13 +1,18 @@
 from turtle import Turtle
 
 STRATING_POSITION = [(0,0), (-20,0), (-40,0)]       # Create a constant (block capital)
-MOVE_DISTANCE = 20  
+MOVE_DISTANCE = 20 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0 
 
 class Snake:
     
     def __init__(self):
         self.segment = []
         self.create_snake()
+        self.head = self.segment[0]                 # To get the snake direction on key press
     
     def create_snake(self):
         # Create a 20px heigt and 60px width snake 
@@ -23,16 +28,21 @@ class Snake:
             new_x = self.segment[seg_num - 1].xcor()               
             new_y = self.segment[seg_num - 1].ycor() 
             self.segment[seg_num].goto(new_x, new_y)         # Give axis values each time it moves it follows the first square
-        self.segment[0].forward(MOVE_DISTANCE) 
-    
+        self.head.forward(MOVE_DISTANCE) 
+
+    # To get the snake direction on key press    
     def up(self):
-        self.segment[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)                          # snake can't go up when it going down
     
     def down(self):
-        self.segment[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.segment[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.segment[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
